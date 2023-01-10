454b3e24627a1f2230519dff9a75c38effb75eff
mihawb <michalbanaszczak@gmail.com>
2023-01-10 20:38:17 +0100

    storage acc key hidden

--- chelp-backend/index.js
+++ chelp-backend/index.js
@@ -9,12 +9,11 @@ const ejs = require('ejs');
 
 const app = express()
 const port = process.env.PORT || 4000
+const STORAGE_ACCOUNT_KEY = process.env.STORAGE_ACCOUNT_KEY || null
 const corsOptions = {
 	origin: process.env.CLIENT_URL || 'http://localhost:3000',
 	optionSuccessStatus: 200
 }
-
-const STORAGE_ACCOUNT_KEY = "DefaultEndpointsProtocol=https;AccountName=chesshelpersa;AccountKey=21K/+dqHL/f3Wl8IWyo30jYb1n8uqj8I3ULVqWSyLoKM4VL3Y2ICGOyFwp+q9iHf4Xud+83kdBu/+AStuj2kXQ==;EndpointSuffix=core.windows.net"
 
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(fileUpload())
@@ -34,8 +33,9 @@ app.post('/upload', (req, res) => {
 		return res.sendStatus(400)
 	}
 
-	// send img to blob storage
-	uploadImageToStorageAccount(image)
+	// send img to blob storage only from prod
+	if (STORAGE_ACCOUNT_KEY)
+		uploadImageToStorageAccount(image)
 
 	// call to containers here, for now sending stub
 	const resFen = { fen: image.name }
